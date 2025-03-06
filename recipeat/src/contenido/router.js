436e71ce1b284e922014365aa6510eac8d62fe03
Gabriel Caliman <145814425+nicocaliman@users.noreply.github.com>
2025-03-06 20:01:43 +0100

    Update router.js

--- recipeat/src/contenido/router.js
+++ recipeat/src/contenido/router.js
@@ -7,8 +7,4 @@ contenidoRouter.get('/normal', viewContenidoNormal);
 
 contenidoRouter.get('/admin', viewContenidoAdmin);
 
-contenidoRouter.get('/', (req, res) => {
-    res.render('paginas/home', { session: req.session });
-});
-
 export default contenidoRouter;
