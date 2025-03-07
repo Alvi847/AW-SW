9c610551571efc4d1a3a1e2d68441cd3ca37e8cd
alexarias8 <alejag29@ucm.es>
2025-03-07 19:50:59 +0100

    updateReceta hecho

--- recipeat/src/receta/controllers.js
+++ recipeat/src/receta/controllers.js
@@ -91,7 +91,9 @@ export function updateReceta(req, res) {
     const id = req.params.id;
     const { nombre, descripcion, likes } = req.body;
 
-    const recetaExistente = new Receta(nombre, descripcion, likes, id);
+    const likesFinal = likes ? likes : 0;
+
+    const recetaExistente = new Receta(nombre, descripcion, likesFinal, id);
     Receta.updateReceta(recetaExistente);
 
     res.redirect('/receta/listaRecetas');
