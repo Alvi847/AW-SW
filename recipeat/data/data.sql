42b77fe00bbc71bf8e9b236bbe578dabaa29e190
Alvi847 <alcepeda@ucm.es>
2025-03-31 19:47:15 +0200

    Subida del data.sql y cambio en la URL del formulario de registro debido al cambio de URLs en el router

--- recipeat/data/data.sql
+++ recipeat/data/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, foto) VALUES (3, 'Pizza Margarita', 'Deliciosa pizza italiana con tomate, mozzarella y albahaca.', 11, 'Gabriel', 'Ponemos un cuenco (apto para horno) con agua, para que tenga algo de humedad cuando vayamos a hornearla. Una vez que tenemos la masa de la pizza preparada, ya extendida en papel de horno, preparamos el relleno.', NULL);
-INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, foto) VALUES (4, 'Tacos al Pastor', 'Tortillas rellenas de cerdo marinado con piña y especias.', 20, NULL, NULL, NULL);
-INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, foto) VALUES (5, 'Sushi Roll', 'Rollos de sushi con salmón, aguacate y arroz.', 15, NULL, NULL, NULL);
-INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, foto) VALUES (6, 'Ensalada César', 'Ensalada con lechuga, pollo, crutones y aderezo César.', 8, NULL, NULL, NULL);
-INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, foto) VALUES (7, 'fabada', 'fabada', 0, NULL, NULL, NULL);
-INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, foto) VALUES (8, 'fabada', 'sdaafsasfa', 0, NULL, NULL, NULL);
-INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, foto) VALUES (9, 'Lasaña', 'Una lasaña de Usuario', 4, 'user', NULL, NULL);
-INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, foto) VALUES (18, 'MUSAKA', 'La receta tradicional de musaka o moussaka se hace con carne picada de cordero, pero si lo preferís lo podéis hacer con carne picada de ternera o de cerdo, aunque en este caso vuestra receta tendrá menos sabor.', 0, NULL, NULL, NULL);
+INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, imagen) VALUES (3, 'Pizza Margarita', 'Deliciosa pizza italiana con tomate, mozzarella y albahaca.', 11, 'Gabriel', 'Ponemos un cuenco (apto para horno) con agua, para que tenga algo de humedad cuando vayamos a hornearla. Una vez que tenemos la masa de la pizza preparada, ya extendida en papel de horno, preparamos el relleno.', NULL);
+INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, imagen) VALUES (4, 'Tacos al Pastor', 'Tortillas rellenas de cerdo marinado con piña y especias.', 20, NULL, NULL, NULL);
+INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, imagen) VALUES (5, 'Sushi Roll', 'Rollos de sushi con salmón, aguacate y arroz.', 15, NULL, NULL, NULL);
+INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, imagen) VALUES (6, 'Ensalada César', 'Ensalada con lechuga, pollo, crutones y aderezo César.', 8, NULL, NULL, NULL);
+INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, imagen) VALUES (7, 'fabada', 'fabada', 0, NULL, NULL, NULL);
+INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, imagen) VALUES (8, 'fabada', 'sdaafsasfa', 0, NULL, NULL, NULL);
+INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, imagen) VALUES (9, 'Lasaña', 'Una lasaña de Usuario', 4, 'user', NULL, NULL);
+INSERT INTO Recetas (id, nombre, descripcion, likes, user, modo_preparacion, imagen) VALUES (18, 'MUSAKA', 'La receta tradicional de musaka o moussaka se hace con carne picada de cordero, pero si lo preferís lo podéis hacer con carne picada de ternera o de cerdo, aunque en este caso vuestra receta tendrá menos sabor.', 0, NULL, NULL, NULL);
 
 
 INSERT INTO Usuarios (id, username, password, rol, nombre, email) VALUES (1, 'user', '$2b$10$JdCg8yL3rRkkr.hhx1rjqOe30F9lhBlqA1sjYJW6ymzYExvQFHyjy', 'U', 'Usuario', NULL);
